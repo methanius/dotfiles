59c8d04f86e648eb9704371fe6e9aef546517848
ClausNMadsen <claus.normann@hotmail.com>
2024-04-18 20:35:37 +0200

    First working home manager from unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "My first Home Manager flake";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";

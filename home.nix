3973d9273181b10f0f2240f00f1e0f686eb42d8d
ClausNMadsen <claus.normann@hotmail.com>
2024-04-18 21:14:57 +0200

    Adding ripgrep

--- home.nix
+++ home.nix
@@ -2,6 +2,9 @@
     home.username = "claus";
     home.homeDirectory = "/home/claus";
     home.stateVersion = "22.11";
+    home.packages = [
+        pkgs.ripgrep
+    ];
 
     programs.home-manager.enable = true;
 }

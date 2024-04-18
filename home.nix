1b5f5764857435e988505c3ab0ef421cd49d92de
ClausNMadsen <claus.normann@hotmail.com>
2024-04-18 21:18:36 +0200

    Move home-manager line to tope of file

--- home.nix
+++ home.nix
@@ -1,10 +1,9 @@
 {pkgs, ...}: {
+    programs.home-manager.enable = true;
     home.username = "claus";
     home.homeDirectory = "/home/claus";
     home.stateVersion = "22.11";
     home.packages = [
         pkgs.ripgrep
     ];
-
-    programs.home-manager.enable = true;
 }

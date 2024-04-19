96e892ed3eae4de76f751fb551ca72498c3f7081
ClausNMadsen <claus.normann@hotmail.com>
2024-04-19 18:55:42 +0200

    Second round of ZSH custom

--- home.nix
+++ home.nix
@@ -17,6 +17,9 @@
             autosuggestion = {
                 enable = true;
             };
+            syntaxHighlighting = {
+                enable = true;
+            };
             completionInit = "autoload -Uz compinit";
             defaultKeymap = "vicmd";
             history = {
@@ -35,6 +38,17 @@
                 ls = "eza";
                 cat = "bat";
             };
+            plugins = [
+            {
+                name = "zsh-expand";
+                src = pkgs.fetchFromGitHub {
+                    owner = "MenkeTechnologies";
+                    repo = "zsh-expand";
+                    rev = "v5.2.0";
+                    sha256 = "sha256-pcYYTQsh2c57U7kVYgCDMi7Z4lAjncQzapfPpTRgKZI=";
+                };
+            }
+            ];
         };
         atuin = {
             enable = true;

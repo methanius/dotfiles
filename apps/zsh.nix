74d5802b8cbbeefa1680df17e6b09c5af79b8a00
ClausNMadsen <claus.normann@hotmail.com>
2024-04-26 20:38:16 +0200

    Refactor ZSH to own folder

---
+++ apps/zsh.nix
@@ -0,0 +1,42 @@
+{pkgs, ... }:
+{
+    programs.zsh = {
+        enable = true;
+        autosuggestion = {
+            enable = true;
+        };
+        syntaxHighlighting = {
+            enable = true;
+        };
+        completionInit = "autoload -Uz compinit";
+        defaultKeymap = "vicmd";
+        autocd = true;
+        history = {
+            expireDuplicatesFirst = true;
+            extended = true;
+            ignoreDups = true;
+            ignoreSpace = true;
+            save = 100000;
+            share = true;
+            size = 100000;
+        };
+        shellAliases = {
+            vi = "nvim";
+            vim = "nvim";
+            go = "xdg-open";
+            ls = "eza";
+            cat = "bat";
+        };
+        plugins = [
+        {
+            name = "zsh-expand";
+            src = pkgs.fetchFromGitHub {
+                owner = "MenkeTechnologies";
+                repo = "zsh-expand";
+                rev = "v5.2.0";
+                sha256 = "sha256-pcYYTQsh2c57U7kVYgCDMi7Z4lAjncQzapfPpTRgKZI=";
+            };
+        }
+        ];
+    };
+}

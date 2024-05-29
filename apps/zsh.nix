25945df3a2c8dedc0bd24cb7d17b843a09c73f58
ClausNMadsen <claus.normann@hotmail.com>
2024-05-29 22:02:32 +0200

    zsh: update shell aliases

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -22,9 +22,13 @@
         shellAliases = {
             vi = "nvim";
             vim = "nvim";
-            go = "xdg-open";
             ls = "eza";
             cat = "bat";
+            gsu = "git submodule update --init --recursive";
+            gss = "git submodule sync --recursive";
+            cmlb = "cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=on;cmake --build build -j15";
+            cmlbrel = "cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=Release;cmake --build build -j15";
+            cmlbreldeb = "cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo;cmake --build build -j15";
         };
         plugins = [
         {

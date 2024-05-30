70f4d18f6ac0105a20c1a462cf9db840fcc4f243
Claus Normann Wibrand <cnm@chora.dk>
2024-05-30 08:34:50 +0200

    nvim: temporarily remove symlinking

--- home.nix
+++ home.nix
@@ -78,9 +78,9 @@ in
         };
     };
     home.file = {
-        ".config/nvim" = {
-            source = config.lib.file.mkOutOfStoreSymlink "/home/claus/dotfiles/config/nvim";
-        };
+        # ".config/nvim" = {
+        #     source = config.lib.file.mkOutOfStoreSymlink "/home/claus/dotfiles/config/nvim";
+        # };
         ".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink "/home/claus/dotfiles/config/starship.toml";
     };
     xdg.configFile."zsh/completions/nix.zsh".source = "${pkgs.nix}/share/zsh/vendor_completions.d/nix.zsh";

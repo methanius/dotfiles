12a035c43aa3c1d602f727d9f07a1ac999c3eb1b
Claus Normann Wibrand <cnm@chora.dk>
2024-06-28 10:34:01 +0200

    nvim: configured gitsigns as per example

--- config/nvim/lua/arbejde/plugins/git.lua
+++ config/nvim/lua/arbejde/plugins/git.lua
@@ -12,8 +12,34 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = function ()
-      require("gitsigns").setup()
-    end
-  }
+    config = function()
+      local gitsigns = require("gitsigns")
+      gitsigns.setup({
+        on_attach = function()
+          vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview git hunk" })
+          vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage git hunk" })
+          vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset git hunk" })
+          vim.keymap.set("v", "<leader>hs", function()
+            gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end, { desc = "Stage git hunk"})
+          vim.keymap.set("v", "<leader>hr", function()
+            gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end, { desc = "Reset git hunk"})
+          vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer to git"})
+          vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Undo stage git hunk"})
+          vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Git reset entire buffer"})
+          vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview git hunk"})
+          vim.keymap.set("n", "<leader>gb", function()
+            gitsigns.blame_line({ full = true })
+          end, { desc = "Git blame line" })
+          vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle git blame line"})
+          vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, { desc = "Git diff this"})
+          vim.keymap.set("n", "<leader>hD", function()
+            gitsigns.diffthis("~")
+          end, { desc = "Git diff all"})
+          vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Gitsigns toggle deleted"})
+        end,
+      })
+    end,
+  },
 }
--- config/nvim/lua/arbejde/plugins/which_key.lua
+++ config/nvim/lua/arbejde/plugins/which_key.lua
@@ -35,6 +35,7 @@ return {
       ["<leader>p"] = { name = "+project" },
       ["<leader>g"] = { name = "+git" },
       ["<leader>t"] = { name = "+toggle" },
+      ["<leader>h"] = { name = "+git hunk" },
       ["<leader>n"] = { name = "+neogen" },
     })
   end,

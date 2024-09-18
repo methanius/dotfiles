1a490650200d745a633c4b3ad510830465721aa2
Claus Normann Wibrand <cnm@chora.dk>
2024-09-18 09:36:06 +0200

    nvim: update lualine

--- config/nvim/lua/arbejde/plugins/lualine.lua
+++ config/nvim/lua/arbejde/plugins/lualine.lua
@@ -1,80 +1,86 @@
-local clients_lsp = function()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local clients = vim.lsp.get_clients({ bufnr = bufnr })
-  if clients == nil then
-    return ""
-  end
-  local c = {}
-  for _, client in pairs(clients) do
-    table.insert(c, client.name)
-  end
-  return "\u{f085} " .. table.concat(c, "|")
-end
-
-local arrowline = function()
-  local arrow = require("arrow.statusline")
-  return arrow.text_for_statusline_with_icons()
-end
-
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   dependencies = {
     { "nvim-tree/nvim-web-devicons", opt = true },
     "otavioschwanck/arrow.nvim",
+    "stevearc/aerial.nvim",
+    "folke/trouble.nvim",
   },
   config = true,
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = "auto",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
-      },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      },
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = {
-        { "branch" },
-        { "diff" },
-        { "diagnostics" },
-      },
-      lualine_c = {
-        {
-          "filename",
-          path = 0,
+  opts = function()
+    local clients_lsp = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      local clients = vim.lsp.get_clients({ bufnr = bufnr })
+      if clients == nil then
+        return ""
+      end
+      local c = {}
+      for _, client in pairs(clients) do
+        table.insert(c, client.name)
+      end
+      return "\u{f085} " .. table.concat(c, "|")
+    end
+
+    local arrowline = function()
+      local arrow = require("arrow.statusline")
+      return arrow.text_for_statusline_with_icons()
+    end
+
+    return {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
         },
-        {
-          arrowline,
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
         },
-        { "aerial", sep = " >" },
       },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = {},
-      lualine_z = { clients_lsp },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = { "mason", "lazy", "oil", "fugitive" },
-  },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          { "branch" },
+          { "diff" },
+          { "diagnostics" },
+        },
+        lualine_c = {
+          {
+            "filename",
+            path = 0,
+          },
+          {
+            arrowline,
+          },
+          {
+            "aerial",sep = " >",
+          },
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = {},
+        lualine_z = { clients_lsp },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = { "mason", "lazy", "oil", "fugitive", "trouble", },
+    }
+  end,
 }

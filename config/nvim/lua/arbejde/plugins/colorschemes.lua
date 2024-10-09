d051a42ba41235f02cf1525eaa6971fa3ecaaaf0
Claus Normann Wibrand <cnm@chora.dk>
2024-10-09 12:42:36 +0200

    nvim: change Kanagawa setup

--- config/nvim/lua/arbejde/plugins/colorschemes.lua
+++ config/nvim/lua/arbejde/plugins/colorschemes.lua
@@ -23,37 +23,38 @@ return {
     },
     lazy = false,
     priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        compile = true,
-        undercurl = true,
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = false },
-        typeStype = {},
-        transparent = true,
-        dimInactive = false,
-        terminalColors = true,
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-            TelescopeTitle = { bg = "none", fg = theme.ui.special, bold = true },
-            TelescopeBorder = { bg = "none" },
-          }
-        end,
-        colors = {
-          theme = { all = { ui = { bg_gutter = "none" } } },
-        },
-        background = {   -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
+    opts = {
+      compile = true,
+      undercurl = true,
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = false },
+      typeStype = {},
+      transparent = true,
+      dimInactive = false,
+      terminalColors = true,
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          TelescopeTitle = { bg = "none", fg = theme.ui.special, bold = true },
+          TelescopeBorder = { bg = "none" },
+        }
+      end,
+      colors = {
+        theme = { all = { ui = { bg_gutter = "none" } } },
+      },
+      background = {   -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+      },
+    },
+    config = function(_, opts)
       -- vim.cmd(":KanagawaCompile")
+      require("kanagawa").setup(opts)
       vim.cmd.colorscheme("kanagawa")
     end,
   },

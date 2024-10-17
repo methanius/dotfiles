04b786b3a5b4f1a59f81385aa90a2aa26843831e
Claus Normann Wibrand <cnm@chora.dk>
2024-10-17 09:58:02 +0200

    nvim: treesitter home tools reconfigured

--- config/nvim/lua/arbejde/plugins/treesitter_home_tools.lua
+++ config/nvim/lua/arbejde/plugins/treesitter_home_tools.lua
@@ -1,21 +1,25 @@
 return {
   {
     "methanius/treesitter_home_tools.nvim",
-    -- dir = "~/project/treesitter_home_tools.nvim",
-    -- dev = true,
-    config = function()
-      local TSHTools = require("treesitter_home_tools")
-      TSHTools.setup({ enable_toggle_boolean = true })
-      vim.keymap.set("n", "<leader>tb", function()
-        TSHTools.toggle_next_bool()
-      end, {
+    opts = {
+      enable_toggle_boolean = true,
+      create_usercommands = true,
+    },
+    cmd = {
+      "ToggleNextBool",
+      "TogglePreviousBool"
+    },
+    keys = {
+      {
+        "<leader>tb",
+        "<Cmd>ToggleNextBool<Cr>",
         desc = "Toggle next boolean using Treesitter",
-      })
-      vim.keymap.set("n", "<leader>tB", function()
-        TSHTools.toggle_previous_bool()
-      end, {
+      },
+      {
+        "<leader>tB",
+        "<Cmd>TogglePreviousBool<Cr>",
         desc = "Toggle previous boolean using Treesitter",
-      })
-    end,
+      }
+    },
   },
 }

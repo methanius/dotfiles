32b3d8df190f311d4850f33560345483a69e8ae1
ClausNMadsen <claus.normann@hotmail.com>
2024-11-06 21:12:59 +0100

    i3: symlink config

--- home.nix
+++ home.nix
@@ -118,7 +118,7 @@ in
     xdg.configFile."zsh/completions/nix.zsh".source = "${pkgs.nix}/share/zsh/vendor_completions.d/nix.zsh";
     xdg.configFile."polybar".source = ./config/polybar;
     xdg.configFile."picom".source = config.lib.file.mkOutOfStoreSymlink (builtins.toPath "${homeDirectory}/dotfiles/config/picom");
-    xdg.configFile."i3".source = ./config/i3;
+    xdg.configFile."i3".source = config.lib.file.mkOutOfStoreSymlink (builtins.toPath "${homeDirectory}/dotfiles/config/i3");
     xdg.configFile."rofi".source = ./config/rofi;
     xdg.configFile."rotating_wallpapers.sh".source = ./config/rotating_wallpaper.sh;
     xdg.configFile."wallpapers".source = ./config/wallpapers;

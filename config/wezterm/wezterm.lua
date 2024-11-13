3c943bfefe2be9578fcdb2c6edf9cc28aaec2810
Claus Normann Wibrand <cnm@chora.dk>
2024-11-13 10:49:27 +0100

    wezterm: font style change

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,41 +8,45 @@ local config = {}
 -- help provide clearer error messages
 --
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Font choice, I like liagutures and fun fonts for italics
-config.font = wezterm.font("JetBrainsMono NF")
+config.font = wezterm.font({
+  family = "JetBrainsMono NF",
+  harfbuzz_features = { "zero", "ss02", "ss19", "cv03" },
+  weight="Light"
+})
+
 config.font_rules = {
-    {
-        intensity = "Bold",
-        italic = true,
-        font = wezterm.font({
-            family = "VictorMono NF",
-            weight = "Bold",
-            style = "Italic",
-        }),
-    },
-    {
-        italic = true,
-        intensity = "Half",
-        font = wezterm.font({
-            family = "VictorMono NF",
-            weight = "DemiBold",
-            style = "Italic",
-        }),
-    },
-    {
-        italic = true,
-        intensity = "Normal",
-        font = wezterm.font({
-            family = "VictorMono NF",
-            style = "Italic",
-        }),
-    },
+  {
+    intensity = "Bold",
+    italic = true,
+    font = wezterm.font({
+      family = "VictorMono NF",
+      weight = "Bold",
+      style = "Italic",
+    }),
+  },
+  {
+    italic = true,
+    intensity = "Half",
+    font = wezterm.font({
+      family = "VictorMono NF",
+      weight = "DemiBold",
+      style = "Italic",
+    }),
+  },
+  {
+    italic = true,
+    intensity = "Normal",
+    font = wezterm.font({
+      family = "VictorMono NF",
+      style = "Italic",
+    }),
+  },
 }
 
-config.harfbuzz_features = { "zero" }
 
 -- Appearance
 config.adjust_window_size_when_changing_font_size = false
@@ -50,10 +54,10 @@ config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 -- config.window_padding
 config.font_size = 15
@@ -63,7 +67,7 @@ config.show_tab_index_in_tab_bar = true
 -- remove titlebar and other annoyances from window
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_frame = {
-    font = wezterm.font("VictorMono NF", { weight = "Regular", italic = true }),
+  font = wezterm.font("VictorMono NF", { weight = "Regular", italic = true }),
 }
 
 -- Turn off the annoying bell!!!
@@ -71,64 +75,64 @@ config.audible_bell = "Disabled"
 
 -- Configs for Windows pc only
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    -- Choosing color_scheme
-    config.color_scheme = "zenbones_dark"
+  -- Choosing color_scheme
+  config.color_scheme = "zenbones_dark"
 
-    config.default_domain = "WSL:Ubuntu"
-    -- Reload configuration every 30 minutes
+  config.default_domain = "WSL:Ubuntu"
+  -- Reload configuration every 30 minutes
 
-    config.background = {
-        {
-            source = {
-                File = "C:\\Users\\cnm\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper",
-            },
-            hsb = { brightness = 0.05 },
-            horizontal_align = "Center",
-            vertical_align = "Middle",
-            repeat_x = "NoRepeat",
-            repeat_y = "NoRepeat",
-            -- height = 1600,
-            -- width = 2560,
-        },
-    }
+  config.background = {
+    {
+      source = {
+        File = "C:\\Users\\cnm\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper",
+      },
+      hsb = { brightness = 0.05 },
+      horizontal_align = "Center",
+      vertical_align = "Middle",
+      repeat_x = "NoRepeat",
+      repeat_y = "NoRepeat",
+      -- height = 1600,
+      -- width = 2560,
+    },
+  }
 
-    -- Track when wallpaper changes
-    wezterm.add_to_config_reload_watch_list(
-        "C:\\Users\\cnm\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper"
-    )
+  -- Track when wallpaper changes
+  wezterm.add_to_config_reload_watch_list(
+    "C:\\Users\\cnm\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper"
+  )
 end
 
 -- Configs for linux only
 if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-    config.default_cwd = wezterm.home_dir
+  config.default_cwd = wezterm.home_dir
 
-    -- Appearance background
-    config.window_background_opacity = 0.9
+  -- Appearance background
+  config.window_background_opacity = 0.9
 
-    -- WebGpu was painfully slow on my linux box for whatever reason!
-    config.front_end = "OpenGL"
-    config.enable_wayland = false
-    local theme = require("kanagawa")
-    config.colors = theme.colors
-    config.force_reverse_video_cursor = false
-    config.colors.tab_bar = {
-      background = "rgba(0,0,0,0)",
-      active_tab = {
-        bg_color = "rgba(0,0,0,0)",
-        fg_color = "#c0c0c0",
-        italic = true,
-        intensity = "Bold",
-        underline = "Single",
-      },
-      inactive_tab = {
-        fg_color = "#c0c0c0",
-        bg_color = "rgba(0,0,0,0)",
-        italic = true,
-        intensity = "Half",
-      },
-      new_tab = {
-        bg_color = "rgba(0,0,0,0)",
-        fg_color = "#c0c0c0",
+  -- WebGpu was painfully slow on my linux box for whatever reason!
+  config.front_end = "OpenGL"
+  config.enable_wayland = false
+  local theme = require("kanagawa")
+  config.colors = theme.colors
+  config.force_reverse_video_cursor = false
+  config.colors.tab_bar = {
+    background = "rgba(0,0,0,0)",
+    active_tab = {
+      bg_color = "rgba(0,0,0,0)",
+      fg_color = "#c0c0c0",
+      italic = true,
+      intensity = "Bold",
+      underline = "Single",
+    },
+    inactive_tab = {
+      fg_color = "#c0c0c0",
+      bg_color = "rgba(0,0,0,0)",
+      italic = true,
+      intensity = "Half",
+    },
+    new_tab = {
+      bg_color = "rgba(0,0,0,0)",
+      fg_color = "#c0c0c0",
     },
   }
 end

cb53a7f4973996df5ba9e29a0c19f64134533ef8
Claus Normann Wibrand <cnm@chora.dk>
2024-12-13 13:54:52 +0100

    nvim: add Snacks library to lazydev

--- config/nvim/lua/arbejde/plugins/lazydev.lua
+++ config/nvim/lua/arbejde/plugins/lazydev.lua
@@ -9,11 +9,12 @@ return {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim",        words = { "Snacks" } },
       },
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional completion source for require statements and module annotations
+  {                                        -- optional completion source for require statements and module annotations
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       opts.sources = opts.sources or {}

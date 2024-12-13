3c2a70e61035086d93dae042b8eea53847309b1d
Claus Normann Wibrand <cnm@chora.dk>
2024-12-13 13:56:30 +0100

    nvim: add various mini and snacks functionality

--- config/nvim/lua/arbejde/plugins/snacks.lua
+++ config/nvim/lua/arbejde/plugins/snacks.lua
@@ -26,13 +26,44 @@ return {
       words = {
         enabled = true,
       },
-    },
-    keys = {
-      { "gn",    function() Snacks.words.jump(vim.v.count1, true) end,  desc = "Jump to next LSP reference", mode = "n" },
-      { "gN",    function() Snacks.words.jump(-vim.v.count1, true) end, desc = "Jump to next LSP reference", mode = "n" },
-      { "<C-/>", function() Snacks.terminal() end,                      desc = "Toggle terminal",            mode = { "n", "t" } },
       notifier = {
         enabled = true,
+      },
+      gitbrowse = {
+        enabled = true,
+      },
+      lazygit = {
+        enabled = true,
+      },
+      statuscolumn = {
+        enabled = true,
+      },
+      keys = {
+        { "gn",         function() Snacks.words.jump(vim.v.count1, true) end,  desc = "Jump to next LSP reference",  mode = "n" },
+        { "gN",         function() Snacks.words.jump(-vim.v.count1, true) end, desc = "Jump to next LSP reference",  mode = "n" },
+        { "<C-/>",      function() Snacks.terminal() end,                      desc = "Toggle terminal",             mode = { "n", "t" } },
+        { "<leader>un", function() Snacks.notifier.hide() end,                 desc = "Dismiss All Notifications" },
+        { "<leader>gb", function() Snacks.git.blame_line() end,                desc = "Git Blame Line" },
+        { "<leader>gl", function() Snacks.lazygit.log() end,                   desc = "Lazygit Log (cwd)" },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end,              desc = "Lazygit Current File History" },
+        {
+          "<leader>N",
+          desc = "Neovim News",
+          function()
+            Snacks.win({
+              file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+              width = 0.6,
+              height = 0.6,
+              wo = {
+                spell = false,
+                wrap = false,
+                signcolumn = "yes",
+                statuscolumn = " ",
+                conceallevel = 3,
+              },
+            })
+          end,
+        },
       },
     },
     init = function()

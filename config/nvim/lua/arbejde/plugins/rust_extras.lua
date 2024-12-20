ec1080cba9c9a44fd814c9f5ba4a9c6d12a1c037
ClausNMadsen <claus.normann@hotmail.com>
2024-12-20 20:30:02 +0100

    nvim: blink.cmp capabilities for rustaceanvim

--- config/nvim/lua/arbejde/plugins/rust_extras.lua
+++ config/nvim/lua/arbejde/plugins/rust_extras.lua
@@ -7,5 +7,13 @@ return {
   {
     "mrcjkb/rustaceanvim",
     ft = { "rust" },
+    dependencies = { "saghen/blink.cmp" },
+    init = function()
+      vim.g.rustaceanvim = {
+        server = {
+          capabilities = require("blink.cmp").get_lsp_capabilities()
+        }
+      }
+    end,
   },
 }

54c2ee9e0c1908c9179d659b78dace67e7e44cce
ClausNMadsen <claus.normann@hotmail.com>
2025-01-12 09:39:01 +0100

    nvim: update blink.cmp

--- config/nvim/lua/arbejde/plugins/lsp.lua
+++ config/nvim/lua/arbejde/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
     build = "nix run .#build-plugin",
     dependencies = {
       { "L3MON4D3/LuaSnip", version = "v2.*" },
+      "folke/lazydev.nvim"
     },
     opts = {
       keymap = {
@@ -15,25 +16,16 @@ return {
         ["<C-u>"] = { "scroll_documentation_up", "fallback" },
       },
       appearance = {
-        use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono"
       },
       sources = {
-        default = { "lsp", "path", "luasnip", "buffer", "lazydev" },
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
         providers = {
-          lsp = { fallbacks = { "lazydev" } },
-          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100, },
         },
       },
       snippets = {
-        expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction) require("luasnip").jump(direction) end,
+        preset = "luasnip",
       },
 
       completion = {

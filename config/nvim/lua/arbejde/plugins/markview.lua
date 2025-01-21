606483da3ad47038e855414b86b9af9a5a281648
ClausNMadsen <claus.normann@hotmail.com>
2025-01-21 20:02:29 +0100

    nvim: markview: lazy load on markdown filetype

--- config/nvim/lua/arbejde/plugins/markview.lua
+++ config/nvim/lua/arbejde/plugins/markview.lua
@@ -1,7 +1,7 @@
 return {
   {
     "OXY2DEV/markview.nvim",
-    lazy = false,
+    ft = "markdown",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",

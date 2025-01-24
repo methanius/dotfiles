efe9761b6e8ab660fada45a88acbbf9eda5903e9
ClausNMadsen <claus.normann@hotmail.com>
2025-01-24 22:08:51 +0100

    nvim: mini pairs: don't use, annoying

--- config/nvim/lua/arbejde/plugins/mini.lua
+++ config/nvim/lua/arbejde/plugins/mini.lua
@@ -1,22 +1,4 @@
 return {
-
-  -- auto pairs
-  {
-    "echasnovski/mini.pairs",
-    event = { "InsertEnter", "CmdlineEnter" },
-    opts = {
-      modes = { insert = false, command = true, terminal = true },
-      -- skip autopair when next character is one of these
-      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-      -- skip autopair when the cursor is inside these treesitter nodes
-      skip_ts = { "string" },
-      -- skip autopair when next character is closing pair
-      -- and there are more closing pairs than opening pairs
-      skip_unbalanced = true,
-      -- better deal with markdown code blocks
-      markdown = true,
-    },
-  },
   {
     "echasnovski/mini.surround",
     event = { "InsertEnter", "CmdlineEnter" , "ModeChanged *:[vV\x16]*"},

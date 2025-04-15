616de218810ece7de58c4042176542b0dd213276
ClausNMadsen <claus.normann@hotmail.com>
2025-04-15 10:32:38 +0200

    nvim: lualine: devicons -> mini.icons

--- config/nvim/lua/arbejde/plugins/lualine.lua
+++ config/nvim/lua/arbejde/plugins/lualine.lua
@@ -12,7 +12,6 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     dependencies = {
-      { "nvim-tree/nvim-web-devicons", opt = true },
       "otavioschwanck/arrow.nvim",
       "SmiteshP/nvim-navic",
     },

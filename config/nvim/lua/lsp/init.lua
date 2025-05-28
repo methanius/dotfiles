2225ebd9b1bd995d7109c8f85efe7f2dcfa59a41
ClausNMadsen <claus.normann@hotmail.com>
2025-05-28 11:52:38 +0200

    nvim: lsp: small change in enabling and masonlspconfig

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -6,6 +6,7 @@ local M = {}
 local servers = require("lsp.servers")
 for server, settings in pairs(servers) do
   vim.lsp.config(server, settings)
+  vim.lsp.enable(server)
 end
 ---@alias CoreKeymapPreNaming { [1]: string, [2]: string|function, [3]: string, mode?: string|string[], extra_opts?: vim.keymap.set.Opts}
 ---@alias LSPKeymapSpec CoreKeymapPreNaming|{has?:string|string[], specific_client?:string , cond?:fun():boolean}
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,10 @@ local servers = require("lsp.servers")
 
 return {
   {
+    "smjonas/inc-rename.nvim",
+    opts = {},
+  },
+  {
     "mason-org/mason.nvim",
     cmd = {
       "Mason",
@@ -25,20 +29,22 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     dependencies = {
+      "mason-org/mason.nvim",
       "neovim/nvim-lspconfig",
     },
+    event = { "BufReadPre", "BufNewFile" },
     opts = {
       ensure_installed = vim.tbl_keys(servers or {}),
       automatic_installation = true,
-      automatic_enable = true,
+      automatic_enable = false,
     }
   },
   {
     "neovim/nvim-lspconfig",
+    events = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "mason-org/mason-lspconfig.nvim",
       "saghen/blink.cmp",
-      { "smjonas/inc-rename.nvim", opts = {}, },
     },
   },
 }

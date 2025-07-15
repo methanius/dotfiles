12316d57e0d216e588749dc939e84a0891ca04d7
Claus Normann Wibrand <cln@mft-energy.com>
2025-07-15 07:59:31 +0200

    nvim: update lazydev libraries

--- config/nvim/lua/plugins/lazydev.lua
+++ config/nvim/lua/plugins/lazydev.lua
@@ -2,6 +2,10 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
+    dependencies = {
+      { "gonstoll/wezterm-types", lazy = true },
+      { "Bilal2453/luvit-meta",   lazy = true }, -- optional `vim.uv` typings
+    },
     opts = {
       library = {
         { "lazy.nvim", },
@@ -9,9 +13,13 @@ return {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "luvit-meta/library", words = { "vim%.uv" } },
+
+        -- Snacks types
         { path = "snacks.nvim",        words = { "Snacks" } },
+
+        -- Optional wezterm types
+        { path = "wezterm-types",      mods = { "wezterm" } },
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
 }

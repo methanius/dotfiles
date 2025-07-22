31a7ba73f36a7e6d58a576715b58a03374941e94
Claus Normann Wibrand <cln@mft-energy.com>
2025-07-22 14:36:44 +0200

    nvim: python ft autoactivate .venv

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -5,3 +5,18 @@ vim.keymap.set(
   "<Cmd>w <bar> exec '!python '.shellescape('%')<CR>",
   { desc = "Save and run Python file" }
 )
+
+local function activate_venv()
+  local venv_path = vim.fn.getcwd() .. "/.venv"
+  if vim.fn.isdirectory(venv_path) == 1 then
+    vim.env.virtual_env = venv_path
+    vim.env.path = venv_path .. "/bin:" .. vim.env.PATH
+    vim.g.python_venv = venv_path
+    vim.notify("Activated Python virtual environment: " .. venv_path, vim.log.levels.INFO)
+  end
+end
+
+if not vim.g.python_activated then
+  activate_venv()
+  vim.g.python_activated = true
+end

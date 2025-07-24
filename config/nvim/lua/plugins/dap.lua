1790dbff9fea7094c9df78ef130add0a64f32125
Claus Normann Wibrand <cln@mft-energy.com>
2025-07-24 08:15:57 +0200

    nvim: dap: do not auto open dap-ui on attach

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -22,12 +22,6 @@ return {
 
       local dapui = require("dapui")
       dapui.setup()
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
       dap.listeners.before.event_terminated.dapui_config = function()
         dapui.close()
       end

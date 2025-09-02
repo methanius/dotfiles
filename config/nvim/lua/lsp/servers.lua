bbeec98a12c820176e1c28641a0515b94298bf76
Claus Normann Wibrand <cln@mft-energy.com>
2025-09-02 11:40:11 +0200

    nvim: ty: move autoImport now it's stabilized

--- config/nvim/lua/lsp/servers.lua
+++ config/nvim/lua/lsp/servers.lua
@@ -38,6 +38,8 @@ local M = {
         diagnosticMode = "workspace",
         experimental = {
           rename = true,
+        },
+        completions = {
           autoImport = true,
         },
       },

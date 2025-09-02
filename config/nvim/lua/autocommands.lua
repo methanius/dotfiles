f668b2e3c2a8d8544200d05c1e21a7212e8f30a6
Claus Normann Wibrand <cln@mft-energy.com>
2025-09-02 11:40:11 +0200

    nvim: treesitter start and autoinstall autocommand

--- config/nvim/lua/autocommands.lua
+++ config/nvim/lua/autocommands.lua
@@ -5,3 +5,47 @@ vim.api.nvim_create_autocmd(
   "FileType",
   { desc = "Open files at last edited position", group = FileGroup, pattern = "*", command = 'silent! normal! g`"zv' }
 )
+
+---@param filetype string
+---@return boolean
+local function is_installed(filetype)
+  for lang in vim.iter(require("nvim-treesitter").get_installed()) do
+    if filetype == lang then
+      return true
+    end
+  end
+  return false
+end
+
+local function is_available(filetype)
+  for lang in vim.iter(require("nvim-treesitter").get_available()) do
+    if filetype == lang then
+      return true
+    end
+  end
+  return false
+end
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    callback = function()
+      if not is_available(vim.bo.filetype) then
+        return
+      end
+      if not is_installed(vim.bo.filetype) then
+        require("nvim-treesitter").install(vim.bo.filetype):await(
+          function()
+            vim.treesitter.start()
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+
+        )
+      else
+        vim.treesitter.start()
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+    end
+  })

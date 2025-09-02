64cedb78dd7261cedcd45db45272034b03dcd2b8
Claus Normann Wibrand <cln@mft-energy.com>
2025-09-02 11:40:11 +0200

    nvim: drop treesitter-context

--- config/nvim/lua/plugins/nvim_treesitter.lua
+++ config/nvim/lua/plugins/nvim_treesitter.lua
@@ -1,9 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-context",
-    },
     branch = "main",
     build = ":TSUpdate",
     lazy = false,

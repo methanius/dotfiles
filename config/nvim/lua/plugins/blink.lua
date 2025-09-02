57c91d34ca032d0892177050cdc61ed8f145f04f
Claus Normann Wibrand <cln@mft-energy.com>
2025-09-02 11:40:11 +0200

    nvim: update blink.cmp settings

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -4,15 +4,20 @@ return {
   build = function()
     -- build the fuzzy matcher, wait up to 60 seconds
     -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
-    require('blink.cmp').build():wait(600000)
+    require('blink.cmp').build():on_resolve(
+      function()
+        vim.notify("Installed Rust based blink fuzzy finder", vim.log.levels.INFO)
+        require('blink.cmp.fuzzy').set_implementation('rust')
+        vim.notify("Switched Blink to Rust based fuzzy finder", vim.log.levels.INFO)
+      end):wait(600000)
   end,
   dependencies = {
     { "L3MON4D3/LuaSnip", version = "v2.*" },
     "folke/lazydev.nvim",
     "saghen/blink.lib",
   },
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
     keymap = {
       preset = "default",
@@ -20,6 +25,16 @@ return {
       ["<C-b>"] = { "snippet_backward", "fallback" },
       ["<C-d>"] = { "scroll_documentation_down", "fallback" },
       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+    },
+    cmdline = {
+      completion = {
+        menu = {
+          auto_show = true
+        },
+      },
+    },
+    term = {
+      enabled = true,
     },
     appearance = {
       nerd_font_variant = "mono"

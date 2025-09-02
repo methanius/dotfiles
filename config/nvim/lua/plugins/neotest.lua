5a797c83b8ff7c0bba408e4748923b1e5b8790e5
Claus Normann Wibrand <cln@mft-energy.com>
2025-09-02 11:40:11 +0200

    nvim: add neotest and python adapter

---
+++ config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({
+            pytest_discover_instances = true,
+          }),
+        },
+      })
+    end,
+  },
+}

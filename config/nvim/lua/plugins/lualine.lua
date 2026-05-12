15c36baffad606ccb19b905e02afd994c638ecc9
Claus Normann Wibrand <cln@mft-energy.com>
2026-05-12 08:07:21 +0200

    nvim: clean up snacks dash, add quickfix to lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -75,7 +75,7 @@ return {
         tabline = {},
         winbar = {},
         inactive_winbar = {},
-        extensions = { "lazy", "oil", "fugitive" },
+        extensions = { "lazy", "oil", "fugitive", "quickfix"},
       }
     end,
   },
